528074d3a9c351419cef1e9987a3223eab9a3208
Abhishek <abhishekahuja576@gmail.com>
2024-12-03 20:06:39 +0530

    closing the server

--- sum.js
+++ sum.js
@@ -1,11 +1,14 @@
-const readline = require('readline')
+const readline = require("readline");
 
 const interface = readline.createInterface({
-    input: process.stdin, output: process.stdout
-})
+  input: process.stdin,
+  output: process.stdout,
+});
 
 interface.question("Enter your no1:", (num1) => {
-    interface.question("Enter your no2:", (num2) => {
-        console.log(Number(num1)+Number(num2))
-    })
-})
+  interface.question("Enter your no2:", (num2) => {
+    console.log(Number(num1) + Number(num2));
+
+    interface.close();
+  });
+});
