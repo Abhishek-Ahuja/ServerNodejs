5634ff8ebe133af8be143bd84f3d3fb07883b7e9
Abhishek <abhishekahuja576@gmail.com>
2024-12-03 20:59:29 +0530

    file created data added and appended

--- filesOperation.js
+++ filesOperation.js
@@ -2,8 +2,10 @@
 const fs = require('fs');
 
 console.log("Started reading the file.");
-const buffer = fs.readFileSync("data.txt");
+const buffer = fs.readFileSync("data.txt", {encoding: 'utf8'});
 
-console.log(buffer.toString());
+console.log(buffer);
+
+fs.writeFileSync("order.txt", "i: 1, type: image, quantiiy: 2")
+fs.appendFileSync("order.txt", 'i: 2, type: choclate, quantiiy: 7')
 console.log("Ended reading the file.");
-
