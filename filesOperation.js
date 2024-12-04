aed51133c4b74181b2295a187e449463a1cc56ac
Abhishek <abhishekahuja576@gmail.com>
2024-12-04 06:27:51 +0530

    ERROR Handling

--- filesOperation.js
+++ filesOperation.js
@@ -6,7 +6,21 @@ const buffer = fs.readFileSync("data.txt", {encoding: 'utf8'});
 
 console.log(buffer);
 // To create and add data in order.txt
+try{
 fs.writeFileSync("order.txt", "i: 1, type: image, quantiiy: 2")
+}catch(err){
+    console.log(err.message)
+}
 //To append data in order.txt
+try{
 fs.appendFileSync("order.txt", 'i: 2, type: choclate, quantiiy: 7')
+}catch(err){
+    console.log(err.message)
+}
+//To delete file
+try{
+    fs.unlinkSync("order.tt")
+}catch(err){
+    console.log(err.message)
+}
 console.log("Ended reading the file.");
